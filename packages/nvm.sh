ef4c9ea9390af732b33258db85bfd6911e9a1d54
xeoncross <david@xeoncross.com>
2015-01-02 11:20:46 -0600

    Fixed install bug in nvm.

--- packages/nvm.sh
+++ packages/nvm.sh
@@ -2,7 +2,11 @@
 
 # https://github.com/creationix/nvm
 if [[ ! -e  ~/.nvm ]]; then
-	curl https://raw.githubusercontent.com/creationix/nvm/master/nvm.sh | bash
+	curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+
+	# Reload bash file to make nvm available
+	source ~/.bashrc
+	# source ~/.nvm/nvm.sh
 
 	nvm install 0.10
 	nvm use 0.10
